c28739322362c39317b046ffbf755e506e435424
Sung Jun Hong <sung.j.hong93@gmail.com>
2019-02-18 16:35:51 -0800

    Add: Added offline capability with service worker

--- sw.js
+++ sw.js
@@ -26,3 +26,14 @@ self.addEventListener('install', (event) => {
 		))
 	);
 })
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((res) => {
+			if(res){
+				return res
+			}
+			return fetch(event.request)
+		})
+	)
+})
